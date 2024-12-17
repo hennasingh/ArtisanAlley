85ca8ac9b1493a906cc09a3eb3b788c7b8c3de94
Henna Singh <hennasingh04@gmail.com>
2024-12-17 07:52:10 +0000

    Fix #4:  Setup Skeleton Templates and URLs for path routing

---
+++ artworks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #making it empty screen results in displaying it on the main page
+    path('' , views.artworks, name="artworks"),
+    path('artwork/<str:pk>', views.artwork, name="artwork"),
+]
--- artworks/views.py
+++ artworks/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+artworklist = [
+    {
+    'id':'1',
+    'title': 'Sunshine Arts',
+    'description': 'Acrylic on 10 by 10 canvas'
+    
+    },
+    {
+    'id':'2',
+    'title': 'Two Souls Jewellery',
+    'description': '100% silver jewellery, crafted by hand'
+    },
+    {
+    'id':'3',
+    'title': 'Quilled Arts',
+    'description': 'Paper coil crafts on 10 by 10 canvas'
+    }
+]
+
+def artworks(request):
+    # this is how we pass messages/variables to html page
+    page = 'Artworks'
+    context = {'page': page, 'artworks': artworklist}
+    return render(request, 'artworks/artworks.html', context)
+
+def artwork(request, pk):
+    artworkObj = None
+    for i in artworklist:
+        if i['id'] == pk:
+            artworkObj = i
+    return render(request, 'artworks/single-artwork.html', {'artwork': artworkObj})
