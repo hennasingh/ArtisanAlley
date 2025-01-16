9ee0b8f64477887b725a88ff2076d70c8c23149f
Henna Singh <hennasingh04@gmail.com>
2025-01-16 16:05:05 +0000

    Add artists model/migrations and Config details to access from admin panel

--- ArtisanAlley/urls.py
+++ ArtisanAlley/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('artworks.urls'))
+    path('', include('artworks.urls')),
+    path('artists/', include('artists.urls')),
 ]
