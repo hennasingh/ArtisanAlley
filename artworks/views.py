293999b82c7f8809a40f0c46ba9ac10e1af51a64
Henna Singh <hennasingh04@gmail.com>
2025-01-21 13:38:44 +0000

    Connect edit and delete templates to respective links in account.html

--- artworks/views.py
+++ artworks/views.py
@@ -18,12 +18,15 @@ def artwork(request, pk):
 
 @login_required(login_url="login")
 def createArtwork(request):
+    profile = request.user.profile
     form = ArtworkForm()
 
     if request.method == 'POST':
         form = ArtworkForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            artwork = form.save(commit=False)
+            artwork.owner = profile
+            artwork.save()
             return redirect('artworks')
 
     context = {'form': form}
@@ -32,7 +35,8 @@ def createArtwork(request):
 
 @login_required(login_url="login")
 def updateArtwork(request, pk):
-    artwork = Artworks.objects.get(id=pk)
+    profile = request.user.profile
+    artwork = profile.artwork_set.get(id=pk)
     form = ArtworkForm(instance=artwork)
 
     if request.method == 'POST':
@@ -47,7 +51,8 @@ def updateArtwork(request, pk):
 
 @login_required(login_url="login")
 def deleteArtwork(request, pk):
-    artwork = Artworks.objects.get(id=pk)
+    profile = request.user.profile
+    artwork = profile.artwork_set.get(id=pk)
     if request.method == 'POST':
         artwork.delete()
         return redirect('artworks')
