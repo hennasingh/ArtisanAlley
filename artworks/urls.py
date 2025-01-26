e1340d577124b81dd365c228e94a8fedfee9fbb4
Henna Singh <hennasingh04@gmail.com>
2025-01-26 03:09:40 +0000

    Fixed artworks url that was accidentally deleted

--- artworks/urls.py
+++ artworks/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
 
+    path('' , views.artworks, name="artworks"),
     path('artwork/<str:pk>', views.artwork, name="artwork"),
 
-    path('create-artwork/', views.createArtwork, name="create-artwork"),
+    path(
+        'create-artwork/', 
+        views.createArtwork, 
+        name="create-artwork"),
 
     path(
         'update-artwork/<str:pk>/',
